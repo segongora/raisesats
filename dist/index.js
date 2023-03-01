930c29c0e2f09aa59ab42af2dd4fe5e94a95c4be
Gasppo <garciagb24@gmail.com>
2023-03-01 14:09:44 -0300

    feat: new index

--- dist/index.js
+++ dist/index.js
@@ -3,16 +3,82 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8080;
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
+const token = process.env.WHATSAPP_TOKEN;
+app.use(body_parser_1.default.json());
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+// Accepts POST requests at /webhook endpoint
+app.post("/webhook", (req, res) => {
+    // Parse the request body from the POST
+    let body = req.body;
+    // Check the Incoming webhook message
+    console.log(JSON.stringify(req.body, null, 2));
+    // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+    if (req.body.object) {
+        if (req.body.entry &&
+            req.body.entry[0].changes &&
+            req.body.entry[0].changes[0] &&
+            req.body.entry[0].changes[0].value.messages &&
+            req.body.entry[0].changes[0].value.messages[0]) {
+            let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
+            let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+            let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+            (0, axios_1.default)({
+                method: "POST",
+                url: "https://graph.facebook.com/v12.0/" +
+                    phone_number_id +
+                    "/messages?access_token=" +
+                    token,
+                data: {
+                    messaging_product: "whatsapp",
+                    to: from,
+                    text: { body: "Ack: " + msg_body },
+                },
+                headers: { "Content-Type": "application/json" },
+            }).then((response) => {
+                console.log("Message sent successfully");
+            });
+        }
+        res.sendStatus(200);
+    }
+    else {
+        // Return a '404 Not Found' if event is not from a WhatsApp API
+        res.sendStatus(404);
+    }
 });
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
+// Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests 
+app.get("/webhook", (req, res) => {
+    /**
+     * UPDATE YOUR VERIFY TOKEN
+     *This will be the Verify Token value when you set up webhook
+    **/
+    const verify_token = process.env.VERIFY_TOKEN;
+    // Parse params from the webhook verification request
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+    // Check if a token and mode were sent
+    if (mode && token) {
+        // Check the mode and token sent are correct
+        if (mode === "subscribe" && token === verify_token) {
+            // Respond with 200 OK and challenge token from the request
+            console.log("WEBHOOK_VERIFIED");
+            res.status(200).send(challenge);
+        }
+        else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
 });
-app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 //# sourceMappingURL=index.js.map
