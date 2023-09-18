e75eebb0c36f5205448609b17e1b27669232aa74
Gasppo <garciagb24@gmail.com>
2023-09-18 18:31:57 -0300

    feature: mensajes de tipos varios manejados

--- dist/index.js
+++ dist/index.js
@@ -12,40 +12,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const methods_1 = require("./methods");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const token = process.env.WHATSAPP_TOKEN;
 app.use(body_parser_1.default.json());
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 app.post("/webhook", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    let body = req.body;
+    var _a, _b, _c, _d, _e, _f;
     console.log(JSON.stringify(req.body, null, 2));
     if (req.body.object) {
-        if (req.body.entry &&
-            req.body.entry[0].changes &&
-            req.body.entry[0].changes[0] &&
-            req.body.entry[0].changes[0].value.messages &&
-            req.body.entry[0].changes[0].value.messages[0]) {
-            let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = req.body.entry[0].changes[0].value.messages[0].from;
-            let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
-            let message_id = req.body.entry[0].changes[0].value.messages[0].id;
-            console.log('Publicando email');
-            const bubbleEmail = yield (0, axios_1.default)({
-                method: "POST",
-                url: process.env.BUBBLE_URL,
-                data: {
-                    message: msg_body,
-                    wafrom: from,
-                    message_id: message_id,
-                },
-                headers: { "Content-Type": "application/json" },
-            });
-            console.log('Bubble response: ', bubbleEmail.data);
+        const message = (_f = (_e = (_d = (_c = (_b = (_a = req === null || req === void 0 ? void 0 : req.body) === null || _a === void 0 ? void 0 : _a.entry) === null || _b === void 0 ? void 0 : _b[0]) === null || _c === void 0 ? void 0 : _c.changes) === null || _d === void 0 ? void 0 : _d[0]) === null || _e === void 0 ? void 0 : _e.value) === null || _f === void 0 ? void 0 : _f.messages[0];
+        if (message) {
+            if ((0, methods_1.isTextMessage)(message))
+                yield (0, methods_1.forwardTextMessage)(message);
+            else if ((0, methods_1.isButtonMessage)(message))
+                yield (0, methods_1.forwardButtonMessage)(message);
+            else
+                yield (0, methods_1.forwardGenericMessage)(message);
         }
         res.sendStatus(200);
     }
---
+++ src/methods.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { ButtonMessage, Message, TextMessage } from "./types";
+
+export const isTextMessage = (message: Message): message is TextMessage => message.type === 'text';
+export const isButtonMessage = (message: Message): message is ButtonMessage => message.type === 'button';
+
+const postMessage = async (data: { message: string, wafrom: string, message_id: string }) => {
+    return await axios({
+        method: "POST",
+        url: process.env.BUBBLE_URL,
+        data,
+        headers: { "Content-Type": "application/json" },
+    })
+}
+
+export const forwardButtonMessage = async (message: ButtonMessage) => {
+    const { from, button, id } = message;
+    const bubbleEmail = await postMessage({ message: button.text, wafrom: from, message_id: id })
+
+    console.log('Bubble response: ', bubbleEmail.data)
+}
+
+export const forwardTextMessage = async (message: TextMessage) => {
+    const { from, text, id } = message;
+    const bubbleEmail = await postMessage({ message: text.body, wafrom: from, message_id: id })
+
+    console.log('Bubble response: ', bubbleEmail.data)
+}
+
+export const forwardGenericMessage = async (message: Message) => {
+    const { from, id } = message;
+    const bubbleEmail = await postMessage({ message: JSON.stringify(message), wafrom: from, message_id: id })
+
+    console.log('Bubble response: ', bubbleEmail.data)
+}
